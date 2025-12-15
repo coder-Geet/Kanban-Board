02a4e2eff567b2d3a84a96351f4136466e459116
coder-Geet <geetsharma1905@gmail.com>
2025-12-15 19:59:53 +0530

    Add Kanban Board Project

---
+++ script.js
@@ -0,0 +1,80 @@
+const todo=document.querySelector('#todo');
+const progress=document.querySelector('#progress');
+const done=document.querySelector('#done');
+let draggedElement=null;
+
+const tasks=document.querySelectorAll('.task');
+
+tasks.forEach(task=>{
+    task.addEventListener("drag",(e)=>{
+            // console.log('dragging',e);
+            draggedElement=task;
+    });
+    
+})
+
+
+function addDragEventsOnColumn(column){
+
+    column.addEventListener("dragenter",(e)=>{
+        e.preventDefault();
+        column.classList.add("hover-over"); 
+
+})
+    column.addEventListener("dragleave",(e)=>{
+
+        e.preventDefault();
+        column.classList.remove("hover-over");
+})
+      column.addEventListener("dragover",(e)=>{
+        e.preventDefault();
+});
+
+    column.addEventListener("drop",(e)=>{
+        e.preventDefault();
+        
+        console.log("dropped",draggedElement,column);
+
+        column.appendChild(draggedElement);
+        column.classList.remove("hover-over");  
+
+})
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done); 
+
+// modal related logic 
+const toggleModalButton=document.querySelector('#toggle-modal');
+const modalBg=document.querySelector('.modal .bg');
+const modal=document.querySelector('.modal');
+const addTaskButton=document.querySelector('#add-new-task');
+
+toggleModalButton.addEventListener("click",()=>{
+    modal.classList.toggle("active");
+})
+
+modalBg.addEventListener("click",()=>{
+    modal.classList.remove("active");
+})
+
+addTaskButton.addEventListener("click",()=>{
+    const taskTitle=document.querySelector('#task-title-input').value;
+    const taskDesc=document.querySelector('#task-desc-input').value;
+
+    const div=document.createElement("div");
+
+    div.classList.add("task");
+    div.setAttribute("draggable","true");
+    div.innerHTML=`<h2>${taskTitle}</h2>
+    <p>${taskDesc}</p>
+    <button>Delete</button>`;
+
+    todo.appendChild(div)  
+    
+    div.addEventListener("drag",(e)=>{
+            draggedElement=div;
+    }   );
+    modal.classList.remove("active");
+})
